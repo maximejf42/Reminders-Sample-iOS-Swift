fcdea07992bf37ed8b4ebd14fce248d8734e780d
Chanikya <chanikya.reddy@ibm.com>
2018-09-19 13:43:18 -0700

    Added new scheme and target for Internal Debugging

---
+++ RemindersSwiftApp/RemindersSwiftApp-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  RemindersSwiftApp-Bridging-Header.m
+//  RemindersSwiftAppDev
+//
+//  Created by Chanikya on 6/14/18.
+//  Copyright © 2018 IBM. All rights reserved.
+//
+
+#import <Tealeaf/TLFApplicationHelper.h>
+#import <Tealeaf/TLFPublicDefinitions.h>
+#import <Tealeaf/TLFCustomEvent.h>
